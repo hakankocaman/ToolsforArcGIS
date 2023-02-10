8263a8e3b8d8a5feb66fb69d213412cd955b8476
Hakan KOCAMAN <hakan_kocaman@hotmail.com>
2023-02-11 00:36:03 +0300

    Develop
    Develop

--- RequestForService.py
+++ RequestForService.py
@@ -18,26 +18,20 @@ try:
 except FileNotFoundError:
   print("File coordinate.txt not found!")
 
-#Sorgu sonucu listelenecek başlıklar:
-print ("IP","Ülke","İl","İlçe","Enlem","Boylam")
-#long=-117.205525
-#lat=34.038232
-#lt=str(long) + "," + str(lat)
-#Sorgulanacak ip lere ait liste:
-ip = ["176.55.55.252","176.55.55.252","88.230.177.68","88.230.177.68"]
-APIKEY = ""
-#ip listesinin teker teker web servis üzerinden sorgulanması:
+
+#koordinat listesinin teker teker web servis üzerinden sorgulanması:
 for lonlat in coordinate:
-    lt=str(lonlat.replace(" ", ","))
-    #print(lt)
-    #Bu kısımda yer alan API_KEY'i https://ipstack.com/ adresine üye olarak temin edebilirsiniz.
-    serviceURL = 'https://geocode.arcgis.com/arcgis/rest/services/World/GeocodeServer/reverseGeocode?distance=&langCode=&locationType=&featureTypes=&outSR=&returnIntersection=false&f=pjson&location='+lt  
+  lt=str(lonlat.replace(" ", ","))
+
+  serviceURL = 'https://geocode.arcgis.com/arcgis/rest/services/World/GeocodeServer/reverseGeocode?distance=&langCode=&locationType=&featureTypes=&outSR=&returnIntersection=false&f=pjson&location='+lt  
     
-    #print(serviceURL)
-    r = requests.get(serviceURL)
-    y = json.loads(r.text)
-    #print(y)
-    #print(y["address"],y["location"])
-    yy=y["address"]
-    print(yy["Address"], ","+lt)
-    #print(y["address"],y["country_name"],y["region_name"],y["city"],y["latitude"],y["longitude"]) 
+  #print(serviceURL)
+  r = requests.get(serviceURL)
+  y = json.loads(r.text)
+
+  yy=y["address"]
+  if yy["Address"]=="":
+    yy["Address"]="Adress not found!"
+
+  print(yy["Address"], ", "+lt)
+ 
